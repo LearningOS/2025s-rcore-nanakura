981b271b2dfb4bc984cf53abad3f3eb07f8d084c
WSRer <1749094641@qq.com>
2025-04-01 23:11:26 +0800

    update

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -39,7 +39,7 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 }
 
 // TODO: implement the syscall
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
-    trace!("kernel: sys_trace");
-    -1
+pub fn sys_trace(trace_request: usize, id: usize, data: usize) -> isize {
+    trace!("kernel: sys_trace | request: {}, id: {}, data: {}", trace_request, id, data);
+    0
 }
